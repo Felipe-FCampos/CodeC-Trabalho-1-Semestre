fc8ddc1e27e9f552823c9b8fd74c1b3f6707ae58
Felipe Cardoso <felipecardoso337@gmail.com>
2023-05-26 17:02:07 -0300

    botão pra organizar e bug fix ;=;

---
+++ botaonellaf.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Pressione o botão [Enter] para continuar..." << std::endl;
+    std::cin.ignore();
+
+    std::cout << "Botão pressionado!" << std::endl;
+
+    return 0;
+}
--- trabalho.cpp
+++ trabalho.cpp
@@ -2,27 +2,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <locale.h>
-#include <iostream>
-#include <Windows.h>
 
-using namespace std;
-int button(int argc, int _TCHAR*, int argv[]){
-    MSG msg;
-    //if you add WS_CHILD flag,CreateWindow will fail because there is no parent window.
-    HWND hWnd = CreateWindow(TEXT("button"), TEXT("Easy"), WS_VISIBLE | WS_POPUP,
-        10, 10, 80, 25, NULL, NULL, NULL,  NULL);
 
-    ShowWindow(hWnd, SW_SHOW);
-    UpdateWindow(hWnd);
-
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return (int) msg.wParam;
-}
 
 int welcomeStart(){
 	
@@ -30,7 +11,6 @@ int welcomeStart(){
 	
 	printf("\n \t\t\t\t\t\t\t\tBem-vindo ao organizador de tarefas.\t\t\t\t\t\t\t\t\n");
 	
-	button();
 
 }
 
